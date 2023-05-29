5812e7b1e1b7e057665f1a662d53374c9ed17ca1
diah <halimatus833@gmail.com>
2023-05-29 08:43:08 -0400

    Menggunakan Method Get

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -18,6 +18,14 @@ public class SepedaAksi {
    
     s.cetakinfo();
     
+    System.out.print("Merknya \t :");
+    System.out.println(s.getMerk ());
+    System.out.print("Warnanya \t :");
+    System.out.println(s.getWarna ());
+    System.out.print("Harganya \t :");
+    System.out.println(s.getHarga ());
+    
+    
     
 }
 }

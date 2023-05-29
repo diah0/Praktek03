2149b16c4ae81e1335ce11f777722a72c36148fc
diah <halimatus833@gmail.com>
2023-05-29 08:53:55 -0400

    Atribute dan Methode

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -2,5 +2,10 @@
 package praktek03;
 
 public class SepedaMotor extends Sepeda{
+    private String jenisMotor;
+    
+    void throttle(){
+        System.out.println("NGEEEEENGGG...BRUMM...JEDER");
+    }
     
 }
